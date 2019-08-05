70edf2e99a924184298968308e8801e3cac06281
kaanlab <kaan@live.ru>
2019-08-05 18:01:42 +0300

    Start DutyController an razor component

---
+++ PetrpkuWeb.Server/Controllers/DutyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetrpkuWeb.Server.Data;
+using PetrpkuWeb.Shared.Models;
+
+namespace PetrpkuWeb.Server.Controllers
+{
+    [Route("api/duty")]
+    [ApiController]
+    public class DutyController : ControllerBase
+    {
+        private readonly DbStorageContext _db;
+
+        public DutyController(DbStorageContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("whoisdutytoday")]
+        public ActionResult<Duty> GetWhoIsDutyToday()
+        {
+            var whoIsDuty = _db.Duties
+                .Where(d => d.DayOfDuty.DayOfYear == DateTime.Now.DayOfYear)
+                .Include(u => u.AssignedTo)
+                .FirstOrDefault();
+            return whoIsDuty;
+        }
+    }
+}
