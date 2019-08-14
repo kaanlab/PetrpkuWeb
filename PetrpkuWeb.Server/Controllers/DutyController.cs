733df3679161e0600018072ff053c870d144942b
kaanlab <kaan@live.ru>
2019-08-14 17:53:55 +0300

    Start working on CreateDutyList component

--- PetrpkuWeb.Server/Controllers/DutyController.cs
+++ PetrpkuWeb.Server/Controllers/DutyController.cs
@@ -37,31 +37,14 @@ namespace PetrpkuWeb.Server.Controllers
                 .Where(d => (d.DayOfDuty.Month == selectedMonth && d.DayOfDuty.Year == selectedYear))
                 .Include(u => u.AssignedTo)
                 .ToListAsync();
+        }
 
-            //var currentMonth = new DateTime(selectedYear, selectedMonth, 1);
-            //var startDate = currentMonth.AddDays(1 - (int)currentMonth.DayOfWeek);
-            //var days = Enumerable.Range(0, 42).Select(i => startDate.AddDays(i));
-
-            //var calendarViewList = new List<CalendarView>();
-
-            //foreach (var day in days)
-            //{
-            //    var personsOnDuty = dutyList?
-            //        .GroupBy(e => e.DayOfDuty);
-            //    var duty = personsOnDuty?
-            //        .SingleOrDefault(k => k.Key == day)?
-            //        .Select(v => v)
-            //        .FirstOrDefault();
-
-            //    var calendarView = new CalendarView();
-            //    calendarView.Day = day.Day;
-            //    calendarView.DutyUser = duty?.AssignedTo;
-
-
-            //    calendarViewList.Add(calendarView);
-            //}
-
-            //return calendarViewList;
+        [HttpPost("createdutylist")]
+        public async Task<ActionResult> PostDutyAsync([FromBody] List<Duty> dutyList)
+        {
+            await _db.Duties.AddRangeAsync(dutyList);
+            await _db.SaveChangesAsync();
+            return Ok();
         }
     }
 }
