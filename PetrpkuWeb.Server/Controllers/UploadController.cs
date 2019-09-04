302eae5de663303ad3fd35b266d765f2fb93bb9c
kaanlab <kaan@live.ru>
2019-09-05 00:01:48 +0300

    Update UserProfile component add upload file menu

--- PetrpkuWeb.Server/Controllers/UploadController.cs
+++ PetrpkuWeb.Server/Controllers/UploadController.cs
@@ -6,6 +6,9 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace PetrpkuWeb.Server.Controllers
 {
@@ -18,13 +21,35 @@ namespace PetrpkuWeb.Server.Controllers
         {
             //var file = Request.Form.Files[0];
             //string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-            //string fullPath = Path.Combine("UploadFiles", fileName);
-            var tempFileName = @"UploadFiles/2.jpeg";
+            var tempFileName = Path.GetTempFileName();
+            string fileName = Path.GetRandomFileName().Substring(0,8) + ".jpg";
+            string fullPath = $@"uploadfolder/{fileName}";
+
+            //var tempFileName = @"UploadFiles/2.jpeg";
             await using (var writer = System.IO.File.OpenWrite(tempFileName))
             {
-                await Request.Body.CopyToAsync(writer);
+                await Request.Body.CopyToAsync(writer);               
             }
-            return Ok(tempFileName);
+
+            using (Image image = Image.Load(tempFileName))
+            {
+                if (image.Width < 600)
+                {
+                    image.Mutate(x => x.Resize(image.Width / 4, image.Height / 4));
+                }
+                else if (image.Width > 600 && image.Width < 1000)
+                {
+                    image.Mutate(x => x.Resize(image.Width / 5, image.Height / 5));
+                }
+                else if (image.Width > 1000 && image.Width < 1400)
+                {
+                    image.Mutate(x => x.Resize(image.Width / 6, image.Height / 6));
+                }
+
+                image.Save(fullPath);
+            }
+
+            return Ok(fullPath);
         }
     }
 }
