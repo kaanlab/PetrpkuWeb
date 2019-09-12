6bae07ee0e3b6d5407336f1b2da590f3ce53b025
kaanlab <kaan@live.ru>
2019-09-13 00:22:53 +0300

    Update Article model. Add enum for css class

--- PetrpkuWeb.Shared/Models/Article.cs
+++ PetrpkuWeb.Shared/Models/Article.cs
@@ -6,11 +6,22 @@ namespace PetrpkuWeb.Shared.Models
 {
     public class Article
     {
+        public enum Style
+        {
+            Standard,
+            Warning,
+            Danger,
+            Info
+        }
+
         public int ArticleId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime PublishDate { get; set; }
-        
+        public Style Name { get; set; }
+
+
+
         // relationship
         public int AppUserId { get; set; }
         public AppUser Author { get; set; }
