1e53e183c1b35f49bd87fb057dd506d98b43abd0
kaanlab <kaan@live.ru>
2019-09-16 00:13:19 +0300

    init comit

--- PetrpkuWeb.Server/Controllers/UploadController.cs
+++ PetrpkuWeb.Server/Controllers/UploadController.cs
@@ -25,57 +25,59 @@ namespace PetrpkuWeb.Server.Controllers
             {
                 var result = new List<FileUploadViewModel>();
                 var dirPath = CreateRandomNameDirectory();
-
+                
                 foreach (var file in files)
-                {
+                {                    
                     var extension = Path.GetExtension(file.FileName);
                     var path = Path.Combine(dirPath, file.FileName);
 
                     using (var stream = new FileStream(path, FileMode.Create))
                     {
                         await file.CopyToAsync(stream);
-                    };
 
-                    if (extension.StartsWith(".jp") || extension.StartsWith(".pn"))
-                    {
-                        using (Image image = Image.Load(path))
+                        stream.Position = 0;
+
+                        if (extension.StartsWith(".jp"))
                         {
-                            if (image.Width > 400 && image.Width < 600)
+                            using (Image image = Image.Load(stream))
                             {
-                                image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+                                if (image.Width > 400 && image.Width < 600)
+                                {
+                                    image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+                                }
+                                else if (image.Width > 600 && image.Width < 1000)
+                                {
+                                    image.Mutate(x => x.Resize(image.Width / 3, image.Height / 3));
+                                }
+                                else if (image.Width > 1000 && image.Width < 1400)
+                                {
+                                    image.Mutate(x => x.Resize(image.Width / 4, image.Height / 4));
+                                }
+                                else if (image.Width > 1400 && image.Width < 3000)
+                                {
+                                    image.Mutate(x => x.Resize(image.Width / 5, image.Height / 5));
+                                }
+                                else if (image.Width > 3000 && image.Width < 5000)
+                                {
+                                    image.Mutate(x => x.Resize(image.Width / 6, image.Height / 6));
+                                }
+                                else if (image.Width > 5000)
+                                {
+                                    image.Mutate(x => x.Resize(image.Width / 7, image.Height / 7));
+                                }
+                                //IImageFormat.
+                                image.SaveAsJpeg(stream);
                             }
-                            else if (image.Width > 600 && image.Width < 1000)
-                            {
-                                image.Mutate(x => x.Resize(image.Width / 3, image.Height / 3));
-                            }
-                            else if (image.Width > 1000 && image.Width < 1400)
-                            {
-                                image.Mutate(x => x.Resize(image.Width / 4, image.Height / 4));
-                            }
-                            else if (image.Width > 1400 && image.Width < 3000)
-                            {
-                                image.Mutate(x => x.Resize(image.Width / 5, image.Height / 5));
-                            }
-                            else if (image.Width > 3000 && image.Width < 5000)
-                            {
-                                image.Mutate(x => x.Resize(image.Width / 6, image.Height / 6));
-                            }
-                            else if (image.Width > 5000)
-                            {
-                                image.Mutate(x => x.Resize(image.Width / 7, image.Height / 7));
-                            }
-
-                            image.Save(path);
                         }
-                    }
+                    };
 
                     result.Add(new FileUploadViewModel() { Name = file.FileName, Length = file.Length, Path = path, Extension = extension });
                 }
                 return Ok(result);
             }
-            catch
+            catch(NotSupportedException ex)
             {
-                return BadRequest();
+                return BadRequest(new FileUploadViewModel() { Extension = ex.Message });
             }
         }
         private string CreateRandomNameDirectory()
