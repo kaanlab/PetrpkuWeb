8e44c78fe0c7ea973c6b424f2d56124e44ec3003
kaanlab <kaan@live.ru>
2019-09-18 23:55:24 +0300

    update CreaateArticle upload component

--- PetrpkuWeb.Shared/ViewModels/FileInfoViewModel.cs
+++ PetrpkuWeb.Shared/ViewModels/FileInfoViewModel.cs
@@ -5,20 +5,10 @@ using Microsoft.AspNetCore.Http;
 
 namespace PetrpkuWeb.Shared.ViewModels
 {
-    public class FileDataViewModel
+    public class FileInfoViewModel
     {
         public long Size { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
-        public string Byte64String { get; set; }
-        
-        public byte[] ByteArray { get; set; }
-
-        public int Width { get; set; }
-
-        public int Height { get; set; }
-        //public IFormFile File { get; set; }
-
-
     }
 }
